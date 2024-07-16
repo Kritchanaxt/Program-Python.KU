3ad7cd722375a3ea080e9bc63620adb0f9228307
Kritchanaxt <kritchanatweve@gmail.com>
2024-07-16 14:42:44 +0700

    update

---
+++ Python/Lab-py/830_labpyw04_6630202015_2024/#11.py
@@ -0,0 +1,8 @@
+
+#? 11.Exercise
+
+sum = 0  
+for i in range(10):
+    for j in range(10):
+        print(i,j)
+print(sum)
--- Python/Lab-py/830_labpyw04_6630202015_2024/#9.py
+++ Python/Lab-py/830_labpyw04_6630202015_2024/#9.py
@@ -3,8 +3,17 @@
 a = set([1,2,3,4]) 
 b = set([3,4,5,6])
 
-#! a | b 
-#! a & b
-#! a.issubset(b) 
-#! a - b
+#? 9.2
+a | b #* union
+a & b #* intersection
+a - b #* difference
+a.issubset(b) 
+
+#? 9.3
+t = ("alpha", "beta", "gamma", "delta")
+t[-2]
+t[1:3]
+# t.append("epsilon")
+
+
 
