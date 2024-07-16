3dadb6a3133ca4fe57fc5afa86b7eeeaf8be17a1
Kritchanaxt <kritchanatweve@gmail.com>
2024-07-16 15:03:20 +0700

    update

--- Python/Lab-py/830_labpyw04_6630202015_2024/challenge04.py
+++ Python/Lab-py/830_labpyw04_6630202015_2024/challenge04.py
@@ -1,9 +1,23 @@
 
-n = 5
+#? 11.Exercise
 
+#! Method 1
+n = 5
 def pattern(n):
      for i in  range(n):
         num_start = 2 * i + 1
         print(' ' * (n - i - 1) + '*' * num_start)
 
 pattern(n)
+
+#! Method 2
+n1 = 5
+for i in range(n1):
+    spaces = ' ' * (n - i - 1)
+    stars = '*' * (2 * i + 1)
+    print(spaces + stars)
+
+#! Method 3
+n2 = 5
+for i in range(n2):
+    print('*' * (2 * i + 1))
